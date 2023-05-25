46399076a725be3ae5d9c50554f9e0a73c3315e7
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2023-05-25 17:50:18 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,16 +1,13 @@
 <?php
 
-$host = "127.0.0.1";
+$host = jss.cobf0jxtq3hd.sa-east-1.rds.amazonaws.com";
 $dbname = "jss";
-$username = "root";
-$password = "";
+$username = "Fatuous.nerd_";
+$password = "Emir_jamil1";
 
-$mysqli = new mysqli(hostname: $host,
-                    username: $username,
-                    password: $password,
-                    database: $dbname);
+$mysqli = new mysqli($host, $username, $password, $dbname);
 
-if ($mysqli->connect_errno){
+if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
 }
 
